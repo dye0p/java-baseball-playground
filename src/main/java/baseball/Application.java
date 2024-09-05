c398ad6496d25c98959ccc251319c94e5e8e9ea5
DongYeop <s01054830455@gmail.com>
2024-09-05 14:36:08 +0900

    feat: 게임시작 기능 추가
    - main 클래스에서 게임이 시작되면 BaseBallGameMaster 클래스의 startGame() 메서드가 실행되어 사용자의 숫자 입력을 받고, 컴퓨터의 난수를 생성함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,4 +1,9 @@
 package baseball;
 
+import baseball.controller.BaseBallGameMaster;
+
 public class Application {
+    public static void main(String[] args) {
+        new BaseBallGameMaster().startGame();
+    }
 }
--- src/main/java/baseball/controller/BaseBallGameMaster.java
+++ src/main/java/baseball/controller/BaseBallGameMaster.java
@@ -1,4 +1,20 @@
 package baseball.controller;
 
+import baseball.model.domain.Computer;
+import baseball.view.InputView;
+
 public class BaseBallGameMaster {
+
+    private final InputView inputView;
+    private final Computer computer;
+
+    public BaseBallGameMaster() {
+        this.computer = new Computer();
+        this.inputView = new InputView();
+    }
+
+    public void startGame() {
+        String input = inputView.inputRequest();
+        String secretNumber = computer.createRandomNumber();
+    }
 }

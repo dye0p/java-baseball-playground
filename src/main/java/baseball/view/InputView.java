958814b1172f9ae7e586118cdf2e7ef96c04453c
DongYeop <s01054830455@gmail.com>
2024-09-05 11:33:14 +0900

    feat: 사용자 입력 메서드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,20 @@
 package baseball.view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    public String inputRequest() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력해 주세요 : ");
+
+        return sc.nextLine();
+    }
+
+    public String getGameStatusChoice() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        return sc.nextLine();
+    }
 }

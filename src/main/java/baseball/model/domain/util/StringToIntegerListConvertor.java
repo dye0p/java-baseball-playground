919d7748451808d0d345ddeaa5fce23104b6cf23
DongYeop <s01054830455@gmail.com>
2024-09-06 16:03:26 +0900

    feat: 문자열 변환 클래스 추가
    - 입력된 문자열을 정수형 리스트로 변환한다.

---
+++ src/main/java/baseball/model/domain/util/StringToIntegerListConvertor.java
@@ -0,0 +1,33 @@
+package baseball.model.domain.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringToIntegerListConvertor {
+    private static final int START_INDEX = 0;
+
+    private final List<Integer> convertNubmer = new ArrayList<>();
+
+    public List<Integer> converte(String playerInputNumber) {
+
+        for (int index = START_INDEX; index < playerInputNumber.length(); index++) {
+            char c = getCharacterAt(playerInputNumber, index);
+            int num = convertCharToInt(c);
+            convertNubmer.add(num);
+        }
+        return convertNubmer;
+    }
+
+    private int convertCharToInt(char c) {
+        return Integer.parseInt(convertCharToString(c));
+    }
+
+    private String convertCharToString(char c) {
+        return String.valueOf(c);
+    }
+
+    private char getCharacterAt(String playerInputNumber, int index) {
+        return playerInputNumber.charAt(index);
+    }
+
+}

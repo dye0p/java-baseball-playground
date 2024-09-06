20f25fb54bde472d052fa873e5b566d9db40cd68
DongYeop <s01054830455@gmail.com>
2024-09-06 17:53:12 +0900

    feat: 숫자야구 게임 결과 판독 기능 추가 (일단 구현)
    - 우선 게임 결과를 판독하는 기능 구현
    - 리팩토링 필요

--- src/main/java/baseball/model/domain/Judge.java
+++ src/main/java/baseball/model/domain/Judge.java
@@ -1,4 +1,66 @@
 package baseball.model.domain;
 
+import baseball.model.domain.util.StringToIntegerListConvertor;
+import java.util.List;
+
 public class Judge {
+
+    private int strike;
+    private int ball;
+    private String result;
+
+    private final StringToIntegerListConvertor convertor = new StringToIntegerListConvertor();
+
+    public String calculateStrikeAndBall(String playerInputNumber, List<Integer> computerSecretNumber) {
+
+        List<Integer> playerNumber = convertor.converte(playerInputNumber);
+
+        strike = 0;
+        ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            Integer computer = computerSecretNumber.get(i);
+            Integer player = playerNumber.get(i);
+
+            if (computer.equals(player)) {
+                strike++;
+            }
+
+            if (!computer.equals(player)) {
+                for (int j = 0; j < 3; j++) {
+                    Integer player2 = playerNumber.get(j);
+
+                    if (i != j && computer.equals(player2)) {
+                        ball++;
+                    }
+                }
+            }
+        }
+
+        if (ball >= 1 && strike >= 1) {
+            result = ball + "볼" + " " + strike + "스트라이크";
+        }
+
+        if (ball == 0 && strike >= 1) {
+            result = strike + "스트라이크";
+        }
+
+        if (strike == 0 && ball >= 1) {
+            result = ball + "볼";
+        }
+
+        if (strike == 3) {
+            result = strike + "스트라이크";
+        }
+
+        if (strike == 0 && ball == 0) {
+            result = "낫싱";
+        }
+
+        return result;
+    }
+
+    public boolean isThreeStrike() {
+        return strike == 3;
+    }
 }

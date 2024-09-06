714e90663493953dc27d92c148280d3fdd261599
DongYeop <s01054830455@gmail.com>
2024-09-06 16:27:10 +0900

    feat: RandomNumberCreator 를 이용하여 랜덤 난수를 생성하는 기능 추가

--- src/main/java/baseball/model/domain/Computer.java
+++ src/main/java/baseball/model/domain/Computer.java
@@ -1,4 +1,14 @@
 package baseball.model.domain;
 
+import baseball.model.domain.util.RandomNumberCreator;
+import java.util.List;
+
 public class Computer {
+
+    private final RandomNumberCreator randomNumberCreator = new RandomNumberCreator();
+
+    public List<Integer> createRandomNumber() {
+        return randomNumberCreator.createRandomNumber();
+    }
 }
+

823f7a77677866c9a0ce0d20f6f2c48708406a6b
DongYeop <s01054830455@gmail.com>
2024-09-06 16:36:33 +0900

    feat: 결과화면 출력 추가

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -1,4 +1,12 @@
 package baseball.view;
 
 public class ResultView {
+
+    public void result(String result) {
+        System.out.println(result);
+    }
+
+    public void endGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }

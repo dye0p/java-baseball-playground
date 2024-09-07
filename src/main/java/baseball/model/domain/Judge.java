86ca8425ef6d837e19678dffec6b4f7101e66eca
DongYeop <s01054830455@gmail.com>
2024-09-07 13:43:09 +0900

    refactor: 매직넘버 상수화

--- src/main/java/baseball/model/domain/Judge.java
+++ src/main/java/baseball/model/domain/Judge.java
@@ -5,11 +5,15 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Judge {
+    private static final int MIM_RANGE = 0;
+    private static final int MAX_RANGE = 3;
+    private static final String STRIKE_TEXT = "스트라이크";
+    private static final String BALL_TEXT = "볼";
+    private static final String NOTHING_TEXT = "낫싱";
 
     private int strike;
     private int ball;
     private String result;
-
     private final StringToIntegerListConvertor convertor = new StringToIntegerListConvertor();
 
     public String calculateStrikeAndBall(String playerInputNumber, List<Integer> computerSecretNumber) {
@@ -20,39 +24,39 @@ public class Judge {
         ball = countBall(computerSecretNumber, playerNumber);
 
         if (ball >= 1 && strike >= 1) {
-            result = ball + "볼" + " " + strike + "스트라이크";
+            result = ball + BALL_TEXT + " " + strike + STRIKE_TEXT;
         }
 
         if (ball == 0 && strike >= 1) {
-            result = strike + "스트라이크";
+            result = strike + STRIKE_TEXT;
         }
 
         if (strike == 0 && ball >= 1) {
-            result = ball + "볼";
+            result = ball + BALL_TEXT;
         }
 
         if (strike == 3) {
-            result = strike + "스트라이크";
+            result = strike + STRIKE_TEXT;
         }
 
         if (strike == 0 && ball == 0) {
-            result = "낫싱";
+            result = NOTHING_TEXT;
         }
 
         return result;
     }
 
-    private int countBall(List<Integer> computerSecretNumber, List<Integer> playerNumber) {
-        return (int) IntStream.range(0, 3)
-                .filter(i -> !computerSecretNumber.get(i).equals(playerNumber.get(i)))
-                .flatMap(i -> IntStream.range(0, 3)
-                        .filter(j -> computerSecretNumber.get(i).equals(playerNumber.get(j))))
+    private int countStrike(List<Integer> computerSecretNumber, List<Integer> playerNumber) {
+        return (int) IntStream.range(MIM_RANGE, MAX_RANGE)
+                .filter(i -> computerSecretNumber.get(i).equals(playerNumber.get(i)))
                 .count();
     }
 
-    private int countStrike(List<Integer> computerSecretNumber, List<Integer> playerNumber) {
-        return (int) IntStream.range(0, 3)
-                .filter(i -> computerSecretNumber.get(i).equals(playerNumber.get(i)))
+    private int countBall(List<Integer> computerSecretNumber, List<Integer> playerNumber) {
+        return (int) IntStream.range(MIM_RANGE, MAX_RANGE)
+                .filter(i -> !computerSecretNumber.get(i).equals(playerNumber.get(i)))
+                .flatMap(i -> IntStream.range(MIM_RANGE, MAX_RANGE)
+                        .filter(j -> computerSecretNumber.get(i).equals(playerNumber.get(j))))
                 .count();
     }
 

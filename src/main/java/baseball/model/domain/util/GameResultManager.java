0bd69d75842e6ef2774b81795670958ce4aefeeb
DongYeop <s01054830455@gmail.com>
2024-09-07 15:17:06 +0900

    refactor: 결과 반환 문자열을 StringBuilder 를 사용하도록 변경

--- src/main/java/baseball/model/domain/util/GameResultManager.java
+++ src/main/java/baseball/model/domain/util/GameResultManager.java
@@ -4,24 +4,27 @@ public class GameResultManager {
     private static final String STRIKE_TEXT = "스트라이크";
     private static final String BALL_TEXT = "볼";
     private static final String NOTHING_TEXT = "낫싱";
+    private static final String SPACE = " ";
 
     public String gameResult(int strike, int ball) {
+        StringBuilder result = new StringBuilder();
+
         if (isThreeStrike(strike)) {
-            return strike + STRIKE_TEXT;
+            return result.append(strike).append(STRIKE_TEXT).toString();
         }
         if (isBothBallAndStrike(strike, ball)) {
-            return ball + BALL_TEXT + " " + strike + STRIKE_TEXT;
+            return result.append(ball).append(BALL_TEXT).append(SPACE).append(strike).append(STRIKE_TEXT).toString();
         }
 
         if (isOnlyStrike(strike, ball)) {
-            return strike + STRIKE_TEXT;
+            return result.append(strike).append(STRIKE_TEXT).toString();
         }
 
         if (isOnlyBall(strike, ball)) {
-            return ball + BALL_TEXT;
+            return result.append(ball).append(BALL_TEXT).toString();
         }
 
-        return NOTHING_TEXT;
+        return result.append(NOTHING_TEXT).toString();
     }
 
     private boolean isThreeStrike(int strike) {

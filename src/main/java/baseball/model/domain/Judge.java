4b10dbcd0e331a26478bd33f361b4ca4f595a365
DongYeop <s01054830455@gmail.com>
2024-09-07 14:28:11 +0900

    refactor: 스트라이크, 볼 계산 메서드를 클래스로 리팩토링

--- src/main/java/baseball/model/domain/Judge.java
+++ src/main/java/baseball/model/domain/Judge.java
@@ -1,26 +1,28 @@
 package baseball.model.domain;
 
+import baseball.model.domain.util.CountBall;
+import baseball.model.domain.util.CountStrike;
 import baseball.model.domain.util.StringToIntegerListConvertor;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class Judge {
-    private static final int MIM_RANGE = 0;
-    private static final int MAX_RANGE = 3;
     private static final String STRIKE_TEXT = "스트라이크";
     private static final String BALL_TEXT = "볼";
     private static final String NOTHING_TEXT = "낫싱";
 
     private int strike;
     private int ball;
+
     private final StringToIntegerListConvertor convertor = new StringToIntegerListConvertor();
+    private final CountStrike countStrike = new CountStrike();
+    private final CountBall countBall = new CountBall();
 
     public String calculateStrikeAndBall(String playerInputNumber, List<Integer> computerSecretNumber) {
 
         List<Integer> playerNumber = convertor.converte(playerInputNumber);
 
-        strike = countStrike(computerSecretNumber, playerNumber);
-        ball = countBall(computerSecretNumber, playerNumber);
+        strike = countStrike.countStrike(computerSecretNumber, playerNumber);
+        ball = countBall.countBall(computerSecretNumber, playerNumber);
 
         return gameResult();
     }
@@ -58,19 +60,5 @@ public class Judge {
 
     private boolean isOnlyBall() {
         return strike == 0 && ball >= 1;
-    }
-
-    private int countStrike(List<Integer> computerSecretNumber, List<Integer> playerNumber) {
-        return (int) IntStream.range(MIM_RANGE, MAX_RANGE)
-                .filter(i -> computerSecretNumber.get(i).equals(playerNumber.get(i)))
-                .count();
-    }
-
-    private int countBall(List<Integer> computerSecretNumber, List<Integer> playerNumber) {
-        return (int) IntStream.range(MIM_RANGE, MAX_RANGE)
-                .filter(i -> !computerSecretNumber.get(i).equals(playerNumber.get(i)))
-                .flatMap(i -> IntStream.range(MIM_RANGE, MAX_RANGE)
-                        .filter(j -> computerSecretNumber.get(i).equals(playerNumber.get(j))))
-                .count();
     }
 }
---
+++ src/main/java/baseball/model/domain/util/CountBall.java
@@ -0,0 +1,17 @@
+package baseball.model.domain.util;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class CountBall {
+    private static final int MIM_RANGE = 0;
+    private static final int MAX_RANGE = 3;
+
+    public int countBall(List<Integer> computerSecretNumber, List<Integer> playerNumber) {
+        return (int) IntStream.range(MIM_RANGE, MAX_RANGE)
+                .filter(i -> !computerSecretNumber.get(i).equals(playerNumber.get(i)))
+                .flatMap(i -> IntStream.range(MIM_RANGE, MAX_RANGE)
+                        .filter(j -> computerSecretNumber.get(i).equals(playerNumber.get(j))))
+                .count();
+    }
+}

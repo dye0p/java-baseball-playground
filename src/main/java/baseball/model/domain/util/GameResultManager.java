0ecc71487e4c98376cee12a4262ee1bed1097344
DongYeop <s01054830455@gmail.com>
2024-09-08 13:25:17 +0900

    refactor(GameResultManager): GameResultStrategy의 구현체들을 사용하여 리팩토링
    - 이중 반복문을 피하기 위해서 스트림을 사용하였음.

--- src/main/java/baseball/model/domain/util/GameResultManager.java
+++ src/main/java/baseball/model/domain/util/GameResultManager.java
@@ -1,64 +1,33 @@
 package baseball.model.domain.util;
 
+import baseball.model.domain.util.gameresult.BothBallAndStrikeStrategy;
+import baseball.model.domain.util.gameresult.GameResultStrategy;
+import baseball.model.domain.util.gameresult.NothingStrategy;
+import baseball.model.domain.util.gameresult.OnlyBallStrategy;
+import baseball.model.domain.util.gameresult.OnlyStrikeStrategy;
+import baseball.model.domain.util.gameresult.ThreeStrikeStrategy;
+import java.util.List;
+
 public class GameResultManager {
-    // TODO [2024/09/07] 상수들을 모두 Enum으로 관리할 수 있는지 고민
-    private static final String STRIKE_TEXT = "스트라이크";
-    private static final String BALL_TEXT = "볼";
-    private static final String NOTHING_TEXT = "낫싱";
-    private static final String SPACE = " ";
+    private static final String EXCEPTION_TEXT = "결과가 없습니다.";
 
-    // TODO [2024/09/07] 메서드 라인수 줄이기 고민
+    private final List<GameResultStrategy> strategies;
+
+    public GameResultManager() {
+        strategies = List.of(
+                new ThreeStrikeStrategy(),
+                new BothBallAndStrikeStrategy(),
+                new OnlyStrikeStrategy(),
+                new OnlyBallStrategy(),
+                new NothingStrategy()
+        );
+    }
+
     public String gameResult(int strike, int ball) {
-        StringBuilder result = new StringBuilder();
-
-        if (isThreeStrike(strike)) {
-            return formatThreeStrike(strike, result);
-        }
-        if (isBothBallAndStrike(strike, ball)) {
-            return formatBallAndStrike(strike, ball, result);
-        }
-        if (isOnlyStrike(strike, ball)) {
-            return formatOnlyStrike(strike, result);
-        }
-        if (isOnlyBall(strike, ball)) {
-            return formatOnlyBall(ball, result);
-        }
-        return formayNothing(result);
-    }
-
-    private String formayNothing(StringBuilder result) {
-        return result.append(NOTHING_TEXT).toString();
-    }
-
-    private String formatOnlyBall(int ball, StringBuilder result) {
-        return result.append(ball).append(BALL_TEXT).toString();
-    }
-
-    private String formatOnlyStrike(int strike, StringBuilder result) {
-        return result.append(strike).append(STRIKE_TEXT).toString();
-    }
-
-    private String formatBallAndStrike(int strike, int ball, StringBuilder result) {
-        return result.append(ball).append(BALL_TEXT).append(SPACE).append(strike).append(STRIKE_TEXT).toString();
-    }
-
-    private String formatThreeStrike(int strike, StringBuilder result) {
-        return result.append(strike).append(STRIKE_TEXT).toString();
-    }
-
-    private boolean isThreeStrike(int strike) {
-        return strike == 3;
-    }
-
-    private boolean isBothBallAndStrike(int strike, int ball) {
-        return ball >= 1 && strike >= 1;
-    }
-
-    private boolean isOnlyStrike(int strike, int ball) {
-        return ball == 0 && strike >= 1;
-    }
-
-    private boolean isOnlyBall(int strike, int ball) {
-        return strike == 0 && ball >= 1;
+        return strategies.stream()
+                .filter(strategy -> strategy.isApplicable(strike, ball))
+                .findFirst()
+                .map(strategy -> strategy.resultMessage(strike, ball))
+                .orElseThrow(() -> new IllegalStateException(EXCEPTION_TEXT));
     }
 }
